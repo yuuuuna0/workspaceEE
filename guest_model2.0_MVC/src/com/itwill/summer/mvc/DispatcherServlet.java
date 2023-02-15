eee09ed29ad2a5a4211c1e426329c0b86c09f40a
yuuuuna0 <jyn9306@gmail.com>
2023-02-15 12:01:06 +0900

    ..

--- guest_model2.0_MVC/src/com/itwill/summer/mvc/DispatcherServlet.java
+++ guest_model2.0_MVC/src/com/itwill/summer/mvc/DispatcherServlet.java
@@ -1,9 +1,14 @@
 package com.itwill.summer.mvc;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -50,18 +55,74 @@ public class DispatcherServlet extends HttpServlet {
 		super.init(config);
 		handlerMapping=new HashMap<String, Controller>();
 		/*
-		 << Map<String, Controller> handlerMapping>>
-		 ------------------------------------------------
-		 |key(String)      |      value(Controller객체) |
-		 ------------------------------------------------
-		 |/guest_main.do   |com..GuestMainController객체|	
-		  -----------------------------------------------
-		 |/guest_list.do   |com..GuestListController객체|		
-		  -----------------------------------------------
-		 |/guest_view.do   |com..GuestViewController객체|		
-		 ------------------------------------------------
+		<<web.xml에설정된 파일이름 파라메타값 가져오기>>
+		<servlet>
+		    <servlet-name>dispatcher</servlet-name>
+		    <servlet-class>com.itwill.summer.mvc.DispatcherServlet</servlet-class>
+		    <init-param>
+		    	<param-name>configFile</param-name>
+		    	<param-value>/WEB-INF/guest_controller_mapping.properties</param-value>
+		    </init-param>
+		    <load-on-startup>0</load-on-startup>
+	  	</servlet>
 		 */
-		/*********************************직접생성*********************/
+		//String configFile="/WEB-INF/guest_controller_mapping.properties";
+		String configFile=config.getInitParameter("configFile");
+		String siteRootRealPath= this.getServletContext().getRealPath("/");
+		System.out.println(">> "+siteRootRealPath);
+		String configFilePath=siteRootRealPath+configFile;
+		try {
+			/*
+			 * 설정파일(guest_controller_mapping.properties)을 읽어서 Properties 객체생성 
+			 */
+			InputStream fis=new FileInputStream(configFilePath);
+			Properties controllerMappingProperties=new Properties();
+			controllerMappingProperties.load(fis);
+			/*
+			 <<Properties객체>>
+			 --------------------------------------------
+			 |key(String)      |      value(String)     |
+			 --------------------------------------------
+			 |/guest_main.do   |com..GuestMainController|	
+			  -------------------------------------------
+			 |/guest_list.do   |com..GuestListController|		
+			  -------------------------------------------
+			 |/guest_view.do   |com..GuestViewController|		
+			 --------------------------------------------	
+			*/
+			Set commandKeySet=controllerMappingProperties.keySet();
+			Iterator<String> commandKeyIterator=commandKeySet.iterator();
+			System.out.println("-------설정파일["+configFile+"]을 이용해서 handlerMapping 객체 생성-------");
+			while (commandKeyIterator.hasNext()) {
+				String command=commandKeyIterator.next();
+				String controllerClassName=controllerMappingProperties.getProperty(command);
+				/*
+				 * "com.itwill.guest.controller.GuestMainController"
+				 * Controller 클래스 이름을 사용해서 Controller 객체 생성
+				 * 		1. Controller 클래스 이름을 사용해서 클래스를 메모리에 로딩
+				 * 		2. 메모리에 로딩된 클래스의 기본생성자를 호출해서 객체 생성
+				 */
+				Class controllerClass=Class.forName(controllerClassName);
+				Controller controllerObject= (Controller)controllerClass.newInstance();	//기본생성자호출코드-->자동으로 불러와지는 클래스는 기본생성자를 public으로 꼭 가지고 있어야 하는 이유
+				handlerMapping.put(command, controllerObject);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+		
+		 
+		/*********************************직접생성*********************
+		<< Map<String, Controller> handlerMapping>>
+		------------------------------------------------
+		|key(String)      |      value(Controller객체) |
+		------------------------------------------------
+		|/guest_main.do   |com..GuestMainController객체|	
+		-----------------------------------------------
+		|/guest_list.do   |com..GuestListController객체|		
+		-----------------------------------------------
+		|/guest_view.do   |com..GuestViewController객체|		
+		------------------------------------------------
 		handlerMapping.put("/guest_main.do", new GuestMainController());
 		handlerMapping.put("/guest_list.do", new GuestListController());
 		handlerMapping.put("/guest_view.do", new GuestViewController());
@@ -72,6 +133,7 @@ public class DispatcherServlet extends HttpServlet {
 		handlerMapping.put("/guest_remove_action.do", new GuestRemoveActionController());
 		handlerMapping.put("/guest_error.do", new GuestErrorController());
 		System.out.println(">> init:"+handlerMapping);
+		****************************************************************/
 		
 	}
 	
