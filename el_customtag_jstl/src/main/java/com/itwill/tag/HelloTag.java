92a18eee0be40bd216dbce46db510eb6928b2ca6
yuuuuna0 <jyn9306@gmail.com>
2023-02-17 14:05:51 +0900

    ..

--- el_customtag_jstl/src/main/java/com/itwill/tag/HelloTag.java
+++ el_customtag_jstl/src/main/java/com/itwill/tag/HelloTag.java
@@ -1,5 +1,38 @@
 package com.itwill.tag;
 
-public class HelloTag {
-	
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class HelloTag extends TagSupport{
+	public HelloTag() {
+		System.out.println("1. HelloTag 기본생성자");
+	}
+	@Override
+	public int doStartTag() throws JspException {
+		System.out.println("doStartTag()");
+		try{
+			JspWriter out = pageContext.getOut();
+			String sUserId=(String)pageContext.getSession().getAttribute("sUserId");
+			if(sUserId==null) {
+				sUserId="Guest";
+			}
+			out.println(sUserId+" 님 안뇽하세요<br>");
+		}catch (IOException e) {
+			throw new JspException(e.getMessage());
+		}
+		return SKIP_BODY;
+	}
+	@Override
+	public int doEndTag() throws JspException {
+		try {
+			pageContext.getOut().print("<hr>");
+		}catch (Exception e) {
+			throw new JspException(e.getMessage());
+		}
+		System.out.println("doEndTag()");
+		return EVAL_PAGE;
+	}
 }
--- el_customtag_jstl/src/main/webapp/6.custom_tag.jsp
+++ el_customtag_jstl/src/main/webapp/6.custom_tag.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<%@taglib prefix="itwill" uri="http://www.itwill.co.kr/jsp/simpleTag" %> 
+<%@ taglib prefix="itwill" uri="http://www.itwill.co.kr/jsp/simpleTag" %>
 <%
 	session.setAttribute("sUserId", "guard");
 	request.setAttribute("name", "현빈");
@@ -14,9 +14,13 @@
 <body >
 <h1>custom tag[사용자정의태그]</h1><hr/>
 ---------------hello tag----------------------<br>
-
+<itwill:hello/>
+<itwill:hello/>
 ---------------helloAttr tag----------------------<br>
-
+<itwill:helloAttr irum="김경호"/>
+<itwill:helloAttr irum="정유나"/>
+<itwill:helloAttr irum="${name}"/>
+<itwill:helloAttr irum="${sUserId}"/>
 ---------------if tag[body]----------------------<br>
 
 
